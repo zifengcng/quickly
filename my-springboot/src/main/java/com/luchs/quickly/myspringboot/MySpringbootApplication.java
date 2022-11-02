e7aa6546a830975ba0cc126311b31aa97ef03417
wubaocheng1 <wubaocheng1@jd.com>
2022-11-02 10:26:22 +0800

    my-springboot

---
+++ my-springboot/src/main/java/com/luchs/quickly/myspringboot/MySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.luchs.quickly.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootApplication.class, args);
+    }
+
+}
---
+++ my-springboot/src/test/java/com/luchs/quickly/myspringboot/MySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luchs.quickly.myspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
